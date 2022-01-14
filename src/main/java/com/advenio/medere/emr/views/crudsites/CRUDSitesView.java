99c70c3a66a2049133291ee8c1dd66f35a58a12f
start.vaadin.com <no-reply@vaadin.com>
2022-01-14 11:29:00 +0000

    Generated project

---
+++ src/main/java/com/advenio/medere/emr/views/crudsites/CRUDSitesView.java
@@ -0,0 +1,37 @@
+package com.advenio.medere.emr.views.crudsites;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@PWA(name = "SitesABM", shortName = "SitesABM", enableInstallPrompt = false)
+@Theme(themeFolder = "sitesabm", variant = Lumo.DARK)
+@PageTitle("CRUDSites")
+@Route(value = "edit")
+@RouteAlias(value = "")
+public class CRUDSitesView extends VerticalLayout {
+
+    public CRUDSitesView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
